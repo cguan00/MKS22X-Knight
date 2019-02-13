539178f2b43533331b364e3a2967c1e0dfc878e9
Christy Guan <cguan00@stuy.edu>
2019-02-13 14:41:39 -0500

    added exception throwing in constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -12,6 +12,9 @@ public class KnightBoard{
   // @throws IllegalArgumentException when either parameter is <= 0.
   // Initialize the board to the correct size and make them all 0's
   public KnightBoard(int startingRows,int startingCols){
+    if(startingRows < 0 || startingCols < 0){
+      throw new IllegalArgumentException();
+    }
     board = new int[startingRows][startingCols];
     rows = startingRows;
     cols = startingCols;
