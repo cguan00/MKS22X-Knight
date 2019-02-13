c003267a261d9c20b1e8c4a34ce0b1068efeb0f4
Christy Guan <cguan00@stuy.edu>
2019-02-13 14:41:11 -0500

    tested exception throwing in constructor

--- Driver.java
+++ Driver.java
@@ -3,6 +3,18 @@ public class Driver{
     KnightBoard board1 = new KnightBoard(5,2);
     KnightBoard board2 = new KnightBoard(8,8);
 
+    try{
+      KnightBoard board3 = new KnightBoard(-4,3);
+    } catch (IllegalArgumentException e){
+      System.out.println("cannot have negative numbers as constructor parameters");
+    }
+
+    try{
+      KnightBoard board3 = new KnightBoard(3, -4);
+    } catch (IllegalArgumentException e){
+      System.out.println("cannot have negative numbers as constructor parameters");
+    }
+
     System.out.println(board1);
     System.out.println("");
     System.out.println(board2);
