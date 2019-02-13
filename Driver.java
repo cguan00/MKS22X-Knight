5fb587bbbe6b0b9a5affdd4863ce21f3de1e0792
Christy Guan <cguan00@stuy.edu>
2019-02-13 14:36:12 -0500

    testing out toString

---
+++ Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+  public static void main(String[] args) {
+    KnightBoard board1 = new KnightBoard(5,2);
+    KnightBoard board2 = new KnightBoard(8,8);
+
+    System.out.println(board1);
+    System.out.println("");
+    System.out.println(board2);
+  }
+}
