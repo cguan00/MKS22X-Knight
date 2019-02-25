305f2635a0a8619efd91007bc56cafe9d882d5f9
Christy Guan <cguan00@stuy.edu>
2019-02-25 14:45:36 -0500

    testing fill in outgoing moves method

--- Driver.java
+++ Driver.java
@@ -15,43 +15,45 @@ public class Driver{
     }
 
     System.out.println(board1);
-    System.out.println(board2);
-    System.out.println(board3);
-    System.out.println(board4);
-    System.out.println(board5);
-    System.out.println(board6);
-    System.out.println(board7);
-
-    System.out.println(board1.isEmpty());//should be true
-    System.out.println(board2.isEmpty());//should be true
-    System.out.println(board3.isEmpty());//should be true
-    System.out.println(board4.isEmpty());//should be true
-    System.out.println(board5.isEmpty());//should be true
-    System.out.println(board6.isEmpty());//should be true
-    System.out.println(board7.isEmpty());//should be true
-    System.out.println("");
-
-    System.out.println(board1.solve(0, 0));
-    System.out.println(board1);
-    System.out.println(board2.solve(0, 0));
-    System.out.println(board2);
-    System.out.println(board3.solve(0, 0));//should be false
-    System.out.println(board3);
-    System.out.println(board4.solve(0, 0));
-    System.out.println(board4);
-    System.out.println(board5.solve(0, 0));//should be false
-    System.out.println(board5);
-    System.out.println(board6.solve(0, 0));//should be false
-    System.out.println(board6);
-    System.out.println(board7.solve(0, 0));
-    System.out.println(board7);
-
-    System.out.println(board1.countSolutions(0, 0));
-    System.out.println(board2.countSolutions(0, 0));
-    System.out.println(board3.countSolutions(0, 0));
-    // System.out.println(board4.countSolutions(0, 0));
-    System.out.println(board5.countSolutions(0, 0));
-    // System.out.println(board7.countSolutions(0, 0));
+    board1.fillOutGoingMoves();
+    System.out.println(board1.movesToStringDebug());
+    // System.out.println(board2);
+    // System.out.println(board3);
+    // System.out.println(board4);
+    // System.out.println(board5);
+    // System.out.println(board6);
+    // System.out.println(board7);
+    //
+    // System.out.println(board1.isEmpty());//should be true
+    // System.out.println(board2.isEmpty());//should be true
+    // System.out.println(board3.isEmpty());//should be true
+    // System.out.println(board4.isEmpty());//should be true
+    // System.out.println(board5.isEmpty());//should be true
+    // System.out.println(board6.isEmpty());//should be true
+    // System.out.println(board7.isEmpty());//should be true
+    // System.out.println("");
+    //
+    // System.out.println(board1.solve(0, 0));
+    // System.out.println(board1);
+    // System.out.println(board2.solve(0, 0));
+    // System.out.println(board2);
+    // System.out.println(board3.solve(0, 0));//should be false
+    // System.out.println(board3);
+    // System.out.println(board4.solve(0, 0));
+    // System.out.println(board4);
+    // System.out.println(board5.solve(0, 0));//should be false
+    // System.out.println(board5);
+    // System.out.println(board6.solve(0, 0));//should be false
+    // System.out.println(board6);
+    // System.out.println(board7.solve(0, 0));
+    // System.out.println(board7);
+    //
+    // System.out.println(board1.countSolutions(0, 0));
+    // System.out.println(board2.countSolutions(0, 0));
+    // System.out.println(board3.countSolutions(0, 0));
+    // // System.out.println(board4.countSolutions(0, 0));
+    // System.out.println(board5.countSolutions(0, 0));
+    // // System.out.println(board7.countSolutions(0, 0));
 
 
   }
